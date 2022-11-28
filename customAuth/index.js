ac6f561e4c7785e1ae7a51ce4060037997e2d6ea
seb4594 <65371156+seb4594@users.noreply.github.com>
2022-11-28 14:29:56 -0600

    Login form & logic

--- customAuth/index.js
+++ customAuth/index.js
@@ -23,6 +23,24 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => { 
     res.send("This is the homepage")
 })
+
+app.get('/login', (req, res) => {res.render('login')})
+app.post('/login', async (req, res) => {
+    const { password, username } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+        res.send("Username or Password is wrong")
+    }else { 
+        validPassword = await bcrypt.compare(password, user.password);
+        if (validPassword) {
+            res.send("Logged In")
+        }
+        else { 
+            res.send("Username or Password is wrong")
+        }
+    }
+})
+
 app.get('/register', (req, res) => {res.render('register')})
 app.post('/register', async (req, res) => {
     // Destructure the body 
